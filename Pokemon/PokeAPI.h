3382fc5f73075131a8b8ee9a5877f19bc1b3c6b8
Sam Moore <semoore95@gmail.com>
2014-09-06 02:13:13 -0400

    #hellyeah we got a basic API wrapper

--- Pokemon/PokeAPI.h
+++ Pokemon/PokeAPI.h
@@ -7,7 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Underscore.h"
 
 @interface PokeAPI : NSObject
+
+// Sometimes we get resource uri's as a reference from the API,
+// e.g. in a pokemon['abilities'], each has a resource URI to get
+// said ability, so we have a full URI so we don't use the method below
++ (NSDictionary *)getResponseWithResourceURI:(NSString *)resourceURI;
+
+// The basic API calls for specific objects
++ (NSDictionary *)getTypeWithID:(NSUInteger)id;
+
++ (NSDictionary *)getAbilityWithID:(NSUInteger)id;
+
++ (NSDictionary *)getPokemonWithID:(NSUInteger)id;
+
+// This literally gets all the pokemon, around 500 or 700
++ (NSDictionary *)getPokedex;
 
 @end
--- Pokemon/PokeAPI.m
+++ Pokemon/PokeAPI.m
@@ -8,9 +8,19 @@
 
 #import "PokeAPI.h"
 
+// SM: TODO: refactor obvi
+
+// used to take a resource URI returned by other methods and make a new request
+// + getResponseFromResourceURI
+const NSString *kPokeApiURL = @"http://pokeapi.co";
+
+// used for all other methods
+const NSString *kPokeApiRef = @"http://pokeapi.co/api/v1/";
+
 @implementation PokeAPI
 
-+ (instancetype) sharedInstance
+/* // might not be needed
++ (instancetype)sharedInstance
 {
     static dispatch_once_t pred;
     static PokeAPI *manager = nil;
@@ -20,7 +30,66 @@
     });
     return manager;
 }
+ */
 
+#pragma mark - API methods
 
++ (NSDictionary *)getTypeWithID:(NSUInteger)id
+{
+    return [self dictionaryFromURL:[self requestStringFromType:@"type" withID:id]];
+}
+
++ (NSDictionary *)getAbilityWithID:(NSUInteger)id
+{
+    return [self dictionaryFromURL:[self requestStringFromType:@"ability" withID:id]];
+}
+
++ (NSDictionary *)getPokemonWithID:(NSUInteger)id
+{
+    return [self dictionaryFromURL:[self requestStringFromType:@"pokemon" withID:id]];
+}
+
++ (NSDictionary *)getPokedex
+{
+    return [self dictionaryFromURL:[self requestStringFromType:@"pokedex" withID:1]];
+}
+
++ (NSDictionary *)getResponseWithResourceURI:(NSString *)resourceURI
+{
+    return [self dictionaryFromURL:[kPokeApiURL stringByAppendingString:resourceURI]];
+}
+
+#pragma mark - Helper methods for API
+
++ (NSString *)requestStringFromType:(NSString *)objectType
+                             withID:(NSUInteger)id
+{
+    NSString *apiRefWithType = [[kPokeApiRef stringByAppendingString:objectType] stringByAppendingString:@"/"];
+    return [apiRefWithType stringByAppendingString:[NSString stringWithFormat:@"%li", id]];
+}
+
++ (NSDictionary *)dictionaryFromURL:(NSString *)URL
+{
+    NSError *error = nil;
+    NSData *response = [NSData dataWithContentsOfURL:[NSURL URLWithString:URL]
+                                             options:NSDataReadingMappedAlways
+                                               error:&error];
+    
+    if (error) {
+        NSLog(@"%@", error);
+        return nil;
+    }
+    
+    NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:response
+                                                               options:kNilOptions
+                                                                 error:&error];
+    
+    if (error) {
+        NSLog(@"%@", error);
+        return nil;
+    }
+    
+    return dictionary;
+}
 
 @end
