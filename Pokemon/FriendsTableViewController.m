5e4d91c3a281a39729702c27e43c6beac115a257
Daniel Stepanov <danielstepanov@0587358429.wireless.umich.net>
2014-09-07 04:11:29 -0400

    storyboard

--- Pokemon/FriendsTableViewController.m
+++ Pokemon/FriendsTableViewController.m
@@ -70,6 +70,7 @@
 {
     [super viewDidLoad];
     
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
---
+++ Pokemon/WildPokemonViewController.h
@@ -0,0 +1,13 @@
+//
+//  WildPokemonViewController.h
+//  Pokemon
+//
+//  Created by Daniel Stepanov on 9/7/14.
+//  Copyright (c) 2014 sammoore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WildPokemonViewController : UIViewController
+
+@end
