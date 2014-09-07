f5c9d8afe759af6f8d552e0b35452404e7cfb818
Daniel Stepanov <danielstepanov@0587358429.wireless.umich.net>
2014-09-07 07:48:07 -0400

    storyboard

--- Pokemon/WildPokemonViewController.h
+++ Pokemon/WildPokemonViewController.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface WildPokemonViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *pokemonName;
+@property (weak, nonatomic) IBOutlet UILabel *pokemonLevel;
+@property (weak, nonatomic) IBOutlet UIImageView *pokemonImage;
+- (IBAction)throwPokeball:(id)sender;
+
+- (IBAction)runAway:(id)sender;
 
 @end
--- Pokemon/WildPokemonViewController.m
+++ Pokemon/WildPokemonViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "WildPokemonViewController.h"
+#import "PokeAPI.h"
 
 @interface WildPokemonViewController ()
 
@@ -46,4 +47,38 @@
 }
 */
 
+
+- (void)searchFieldReturned:(UITextField *)sender
+// or IBAction return and id sender but we're triggering this,
+// and 'return' can't trigger an IBAction, so better code completion rules
+{
+    NSDictionary *pokemonRef = [PokeAPI findOnePokemonByString:[sender text]];
+    
+    if (pokemonRef == nil) return;
+    
+    NSDictionary *pokemon = [PokeAPI getResponseWithResourceURI:pokemonRef[@"resource_uri"]];
+    //Pokemon Name
+    NSString *pokedexURI = [[pokemon[@"descriptions"] firstObject] objectForKey:@"resource_uri"];
+    NSDictionary *desc = [[PokeAPI getResponseWithResourceURI:pokedexURI] objectForKey:@"description"];
+    //Pokemon Level
+    NSString *pokedexURI = [[pokemon[@"descriptions"] firstObject] objectForKey:@"resource_uri"];
+    NSDictionary *desc = [[PokeAPI getResponseWithResourceURI:pokedexURI] objectForKey:@"description"];
+    //Pokemon Image
+    NSString *spriteURI = [[pokemon[@"sprites"] firstObject] objectForKey:@"resource_uri"];
+    NSString *spriteImageURL = [@"http://pokeapi.co" stringByAppendingString: [[PokeAPI getResponseWithResourceURI:spriteURI] objectForKey:@"image"]];
+    //Set the above to their outlets
+    _pokemonName.text = [NSString stringWithFormat:@"%@", desc];
+    _pokemonLevel.text = [NSString stringWithFormat:@"%@", num];
+    _pokemonImage.image =[UIImage imageWithData: [NSData dataWithContentsOfURL:[NSURL URLWithString: spriteImageURL]]];
+    
+}
+
+- (IBAction)throwPokeball:(id)sender {
+    
+
+}
+
+- (IBAction)runAway:(id)sender {
+    [self.presentedViewController dismissViewControllerAnimated:YES completion:nil];
+}
 @end
