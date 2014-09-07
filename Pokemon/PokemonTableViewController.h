8dc4ab2494639fa69bb2f5019e6ff3993a9a1ac4
Sam Moore <semoore95@gmail.com>
2014-09-07 03:09:25 -0400

    PokemonTableViewController

---
+++ Pokemon/PokemonTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  PokemonTableViewController.h
+//  Pokemon
+//
+//  Created by Sam Moore on 9/7/14.
+//  Copyright (c) 2014 sammoore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Firebase/Firebase.h>
+#import "FacebookAPI.h"
+#import "Underscore/Underscore.h"
+
+@interface PokemonTableViewController : UITableViewController <FBAPIDelegate>
+
+@end
---
+++ Pokemon/PokemonTableViewController.m
@@ -0,0 +1,167 @@
+//
+//  PokemonTableViewController.m
+//  Pokemon
+//
+//  Created by Sam Moore on 9/7/14.
+//  Copyright (c) 2014 sammoore. All rights reserved.
+//
+
+#import "PokemonTableViewController.h"
+
+@interface PokemonTableViewController ()
+
+@property (strong, nonatomic) Firebase *FBRef;
+@property (strong, nonatomic) NSString *userID;
+@property (strong, nonatomic) NSDictionary *pokemonDict;
+@property (strong, nonatomic) NSArray *dataSource;
+
+@end
+
+@implementation PokemonTableViewController
+
+- (Firebase *)FBRef {
+    if (_FBRef == nil) {
+        _FBRef = [[Firebase alloc] initWithUrl: @"https://mhacks-pokemon.firebaseio.com/pokemon"];
+    }
+    
+    return _FBRef;
+}
+
+- (NSString *)userID {
+    if (_userID == nil) {
+        [FacebookAPI getPublicProfile:self];
+    }
+    
+    return _userID;
+}
+
+- (void)publicProfileReceived:(NSDictionary *)publicProfile
+{
+    _userID = publicProfile[@"id"];
+    [self reloadTable];
+}
+
+- (void)reloadTable
+{
+    if (self.userID != nil && self.pokemonDict != nil)
+    {
+        //Underscore.dictionary(self.pokemonDict)
+    }
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        [FacebookAPI getPublicProfile:self];
+    }
+    return self;
+}
+
+- (void)updatePokemon:(NSDictionary *)dict
+{
+    self.pokemonDict = (NSDictionary *)dict;
+    [self reloadTable];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.FBRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        [self updatePokemon:snapshot.value];
+    } withCancelBlock:^(NSError *error) {
+        NSLog(@"%@", error.description);
+    }];
+    
+    NSLog(@"hi!");
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
